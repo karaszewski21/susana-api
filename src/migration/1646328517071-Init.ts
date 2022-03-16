710846578da98cd4c5ae77ae6e238c85f366d16a
karaszewski21 <pkaraszewski@agora.pl>
2022-03-16 17:59:07 +0100

    first init

--- src/migration/1646328517071-Init.ts
+++ src/migration/1646328517071-Init.ts
@@ -1,14 +1,15 @@
-// import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
-// export class Init1646328517071 implements MigrationInterface {
-//     name = 'Init1646328517071'
+export class Init1646328517071 implements MigrationInterface {
+  name = 'Init1646328517071';
 
-//     public async up(queryRunner: QueryRunner): Promise<void> {
-//         await queryRunner.query(`CREATE TABLE "account_entity" ("id" SERIAL NOT NULL, "login" character varying NOT NULL, "password" character varying NOT NULL, CONSTRAINT "PK_b482dad15becff9a89ad707dcbe" PRIMARY KEY ("id"))`);
-//     }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "account_entity" ("id" SERIAL NOT NULL, "login" character varying NOT NULL, "password" character varying NOT NULL, CONSTRAINT "PK_b482dad15becff9a89ad707dcbe" PRIMARY KEY ("id"))`,
+    );
+  }
 
-//     public async down(queryRunner: QueryRunner): Promise<void> {
-//         await queryRunner.query(`DROP TABLE "account_entity"`);
-//     }
-
-// }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "account_entity"`);
+  }
+}
