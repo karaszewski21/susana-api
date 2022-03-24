ecdef23454c6bb4391aebd92f9108d5ceeef573f
karaszewski21 <pkaraszewski@agora.pl>
2022-03-24 16:59:37 +0100

    add turn

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as Turn from 'node-turn';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const server = new Turn({
+    // set options
+    authMech: 'none',
+    credentials: {
+      username: 'karas123456789',
+    },
+  });
+
+  server.start();
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
